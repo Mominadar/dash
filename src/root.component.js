6963a045356270dd4285b32eaaa4307b9e94ec00
Momina Babar <momina.babar@auxcube.com>
2024-07-24 16:00:03 +0500

    add

--- src/root.component.js
+++ src/root.component.js
@@ -1,3 +1,14 @@
+import * as singleSpa from "single-spa";
+
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <section>{props.name} is mounted!
+  <button onClick={()=>{
+    singleSpa.navigateToUrl("/app1");
+  }}>go to app1</button>
+  <button onClick={()=>{
+    singleSpa.navigateToUrl("/app2");
+  }}>go to app2</button>
+
+  </section>;
 }
